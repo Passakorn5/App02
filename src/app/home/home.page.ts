57f3edc91c50b936e58a2530b0dac86b0f9e94b4
Passakorn <0634198444nan@gmail.com>
2024-08-05 14:24:44 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { trashOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonIcon, IonButton, IonHeader, IonToolbar, IonTitle, IonContent,RouterLink],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({})
+  }
 }
